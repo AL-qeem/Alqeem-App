df968c9be23c114d169180c8312cd66791165341
AL-qeem <info@mr-klean.at>
2025-07-27 22:48:59 +0200

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,1 @@
-import React from 'react'
-
-function App() {
-  return (
-    <div>
-      <h1>🤖 Willkommen bei Alqeem</h1>
-      <p>Dein persönlicher KI-Assistent</p>
-    </div>
-  )
-}
-
-export default App
+// Hier ist die Hauptkomponente mit Push-Logik und PWA-Bereitschaft
