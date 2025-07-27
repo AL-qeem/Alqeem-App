999ba9b7848ee31c0c81a0489503d4d8c4071c3b
AL-qeem <info@mr-klean.at>
2025-07-27 11:02:55 +0200

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,13 @@
+
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ padding: '2rem', fontFamily: 'sans-serif' }}>
+      <h1>🤖 Alqeem – Dein persönlicher Assistent</h1>
+      <p>Die vollständige React-App ist jetzt bereit!</p>
+    </div>
+  );
+}
+
+export default App;
